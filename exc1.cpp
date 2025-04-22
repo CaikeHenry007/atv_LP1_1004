11fc876ed168d4c7d9cf4c6f7f73f35e8b64dfef
Caike Henry <125682254+CaikeHenry007@users.noreply.github.com>
2025-04-22 20:23:16 -0300

    Create exc1.cpp

---
+++ exc1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+	int vetor[10], i=0;
+
+	for(i = 0; i < 10; i++) {
+		printf("Digite um valor para colocar no vetor. \n");
+		scanf("%d", &vetor[i]);
+
+		if (vetor[i] % 2 == 0) {
+			vetor[i] = vetor[i] * 1.05;
+		} else {
+			vetor[i] = vetor[i] * 1.02;
+		}
+	}
+	for(i = 0; i < 10; i++) {
+	    printf("O vetor ficou assim: \n")
+		printf("%d \n", vetor[i]);
+	}
+}
