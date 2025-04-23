6f22371e3f9a3b05220cbfdd4c41579d9ab294c4
Caike Henry <125682254+CaikeHenry007@users.noreply.github.com>
2025-04-22 21:51:05 -0300

    Update exc5.cpp

--- exc5.cpp
+++ exc5.cpp
@@ -2,18 +2,25 @@
 
 main()
 {
-	int i=0, contador = 0;
-	char gabarito[10][1], respostas[10][1];
+	int i=0, contador = 0, gabarito[40], respostas[40];
 
-	for(i = 0; i < 10; i++) {
+	for(i = 0; i < 40; i++) {
 		printf("Digite a resposta correta do gabarito. \n");
+		printf("1 - A \n");
+		printf("2 - B \n");
+		printf("3 - C \n");
+		printf("4 - D \n");
 		scanf("%d", &gabarito[i]);
 	}
-	for(i = 0; i < 10; i++) {
+	for(i = 0; i < 40; i++) {
 		printf("Digite a resposta do candidato. \n");
-		scanf("%d", &respostas[i][1]);
+		printf("1 - A \n");
+		printf("2 - B \n");
+		printf("3 - C \n");
+		printf("4 - D \n");
+		scanf("%d", &respostas[i]);
 		
-		if(respostas[i][1] == gabarito[i][1]) {
+		if(respostas[i] == gabarito[i]) {
 		    contador ++;
 		}
 	}
